f07d71a5d52542f948979982d183f50f909606c9
24331a05j5-harsita <24331a05j5@mvgrce.edu.in>
2025-12-14 12:58:53 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,5 @@
+principal=float(input("enter the principal amount is:"))
+rate=float(input("enter the rate of interest in % is:"))
+time=float(input("enter the time period in years is:"))
+simple_interest=(principal*rate*time)/100
+print("simple interest is:",simple_interest)
